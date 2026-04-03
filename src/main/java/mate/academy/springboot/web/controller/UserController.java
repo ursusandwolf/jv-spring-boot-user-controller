42ed462c7eeb47adeb9638579f163a0a8ad71deb
alex <ursusandwolf@protonmail.com>
2026-04-03 15:56:52 +0300

    UserController - index

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -1,5 +1,17 @@
 package mate.academy.springboot.web.controller;
 
+import java.util.List;
+import mate.academy.springboot.web.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    @GetMapping("/index")
+    public List<User> getUsers() {
+        User user1 = new User(1L, "bob@i.ua");
+        User user2 = new User(2L, "alice@i.ua");
+        return List.of(user1, user2);
+    }
 
 }
