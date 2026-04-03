02dce4efac832541a036d132edc7af74189f78e7
alex <ursusandwolf@protonmail.com>
2026-04-03 16:56:58 +0300

    post and get methods

--- src/main/java/mate/academy/springboot/web/controller/UserController.java
+++ src/main/java/mate/academy/springboot/web/controller/UserController.java
@@ -3,15 +3,21 @@ package mate.academy.springboot.web.controller;
 import java.util.List;
 import mate.academy.springboot.web.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @GetMapping("/index")
-    public List<User> getUsers() {
-        User user1 = new User(1L, "bob@i.ua");
-        User user2 = new User(2L, "alice@i.ua");
-        return List.of(user1, user2);
+    @GetMapping("/users")
+    public List<User> getAll() {
+        return List.of(
+                new User(1L, "bob@i.ua"),
+                new User(2L, "alice@i.ua"));
     }
 
+    @PostMapping("/users")
+    public String save(User user) {
+        return "Product was saved Id: %s, E-mail: %s"
+                .formatted(user.id(), user.email());
+    }
 }
